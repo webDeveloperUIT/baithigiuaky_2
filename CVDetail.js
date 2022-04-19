c2c6d70f93296ba5be2f78bbe946380f2f512042
locpham <loc281202@gmail.com>
2022-04-19 22:25:10 +0700

    fix bug

--- CVDetail.js
+++ CVDetail.js
@@ -1,19 +1,17 @@
-var product = JSON.parse(window.localStorage.getItem("CVtDetail"));
-console.log(CV);
+var cvDetail = JSON.parse(window.localStorage.getItem("cvDetail"));
 document.body.innerHTML = `
-      
       <div class="CV-info">
-        <h1 class="title">${product.TenCV}</h1>
-        <h2 class="Nguoiphutrach">Người phụ trách: ${CV.Nguoiphutrach}</h2>
-        <p class="Noinhanhang"> Nơi nhận hàng: ${CV.Noinhan}</p>
-        <p class="Noigiaohang"> Nơi giao hàng: ${CV.Noigiaohang}</p>
-        <p class="Ngaybatdau"> Ngày bắt đầu: ${CV.Ngaybatdau}</p>
-        <p class="Ngayketthuc"> Ngày kết thúc: ${CV.Ngayketthuc}</p>
+        <h1 class="title">${cvDetail.TenCV}</h1>
+        <h2 class="Nguoiphutrach">Người phụ trách: ${cvDetail.Nguoiphutrach}</h2>
+        <p class="Noinhanhang"> Nơi nhận hàng: ${cvDetail.Noinhanhang}</p>
+        <p class="Noigiaohang"> Nơi giao hàng: ${cvDetail.Noigiaohang}</p>
+        <p class="Ngaybatdau"> Ngày bắt đầu: ${cvDetail.Ngaybatdau}</p>
+        <p class="Ngayketthuc"> Ngày kết thúc: ${cvDetail.Ngayketthuc}</p>
       </div>
     </div>
     <div class="desc">
       <h1>Chi tiết</h1>
-      <p>${CV.Chitiet} </p>
+      <p>${cvDetail.Chitiet} </p>
     </div>
  
 `;
--- app.js
+++ app.js
@@ -1,37 +1,41 @@
 var form = document.querySelector(".form");
-  var container = document.querySelector(".container");
-  var lishCV = [];
-  var showitem = {};
-  const addCV = () => {
-    var formData = new FormData(form);
-    var info = {
-      TenCV: formData.get("TenCV"),
-      Nguoiphutrach: formData.get("Nguoiphutrach"),
-      Noinhan: formData.get("Noinhanhang"),
-      Noinhanhang: formData.get("Noinhanhang"),
-      Ngaybatddau: formData.get("Ngaybatdau"),
-      Chitiet: formData.get("Chitiet"),
-    };
-    lishCV.push(info);
-    console.log(lishCV);
-    var list = lishCV.map((item, index) => {
-      return `
+var container = document.querySelector(".container");
+var listCV = [];
+var showitem = {};
+const addCV = () => {
+  var formData = new FormData(form);
+  var testId = document.querySelector("#TenCV").value;
+  console.log(testId);
+  var info = {
+    TenCV: formData.get("TenCV"),
+    Nguoiphutrach: formData.get("Nguoiphutrach"),
+    Noigiaohang: formData.get("Noigiaohang"),
+    Noinhanhang: formData.get("Noinhanhang"),
+    Ngaybatdau: formData.get("Ngaybatdau"),
+    Ngayketthuc: formData.get("Ngayketthuc"),
+    Chitiet: formData.get("Chitiet"),
+  };
+  listCV.push(info);
+  console.log(listCV);
+  var list = listCV.map((item, index) => {
+    console.log(item.Ngayketthuc);
+    return `
       <li class="CV-item">
             
-            <h1 class="CV-TenCV" ${CV.TenCV} class="desc-link" href="./productItem.html" onclick="getItem(${index})"> </h1>
+            <a class="CV-TenCV"  class="desc-link" href="./CVItem.html" onclick="getItem(${index})"> ${item.TenCV}</a>
             <h2 class="CV-Nguoiphutrach"> Người phụ trách:${item.Nguoiphutrach}</h2>
-            <p class="CV-Noinhanhang"> Nơi nhận hàng:${item.Noinhanhang}</p>
+            <p class="CV-Noinhanhang"> Nơi nhận hàng: ${item.Noinhanhang}</p>
             <p class="CV-Ngaybatdau"> Ngày bắt đầu:${item.Ngaybatdau}</p>
           </li>
           `;
-    });
-    container.innerHTML = list.join("");
-  };
-  const getItem = (index) => {
-    CV = lishCV[index];
-    window.localStorage.setItem("productDetail", JSON.stringify(CV));
-  };
-  // console.log(lishCV);
-  const resetForm = () => {
-    form.reset();
-  };
+  });
+  container.innerHTML = list.join("");
+};
+const getItem = (index) => {
+  CV = listCV[index];
+  window.localStorage.setItem("cvDetail", JSON.stringify(CV));
+};
+// console.log(lishCV);
+const resetForm = () => {
+  form.reset();
+};
